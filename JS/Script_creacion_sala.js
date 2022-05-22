ca9f67ffbb5bb9173ccbe3dfd47f19f8e27df39b
FedeAsin <67613839+FedeAsin@users.noreply.github.com>
2022-05-22 14:09:16 -0300

    Pre-Entrega (22/05)

--- JS/Script_creacion_sala.js
+++ JS/Script_creacion_sala.js
@@ -1,116 +1,127 @@
+// Inicio
+let completar = true;
+while(completar){
+        let instrucciones = "Seleccioná que querés hacer: \n"
+        instrucciones += "\n 1. 🆕 Crear sala";
+        instrucciones += "\n 2. 🔍 Ver salas creadas";
+        instrucciones += "\n 3. ❌ Borrar una sala";
 
-let respueta = confirm ("¿Querés crear una sala nueva?")
+        let respuesta_instrucciones = prompt(instrucciones);
 
-if (respueta){
-    let nombre = prompt("¿Cual va a ser el nombre de la sala?").toUpperCase();
-
-    let piso;
-    piso = validarN(piso, 15, 1,"¿En que piso esta la sala?", "El edificio tiene 15 pisos, elegí otro piso","Elegí un piso entre 1 y 15","Elegiste el piso");
-
-    let capacidad;
-    capacidad = validarN(capacidad, 20, 1, "¿Cuantas personas pueden entrar?", "La salas solo tienen capacidad hasta 20 personas", "La sala se debe reservar minimamente para 1 persona", "Elegiste la cantidad de invitados");
-
-    let proyector;
-    proyector = proyector_detalle(proyector);
-
-
-/////////////////Validar datos/////////////////////////////
-
-
-    do{
-        if (validarDatos(nombre) && validarDatos(capacidad) && validarDatos(piso)){
-
-                let nueva_sala = new Sala (nombre, capacidad, piso, proyector);
-                let confirmar_sala = crear_confirmacion(nueva_sala);
-
-                if (confirmar_sala) {
-                    alert ("Sala " + nueva_sala.mostrar_info() + " creada");
-                    document.write (nueva_sala.mostrar_infoCompleta());
-                    console.log(nueva_sala);
-                }else{
-                    let actualizarDato;
-                    do{
-                        actualizarDato = prompt("¿Que dato queres actualizar? \n - Nombre \n - Piso \n - Capacidad \n - Proyector");
-                        switch (actualizarDato){
-                            case "Nombre":
-                                respuesta_actualizacion = prompt ("¿Cual va a ser el nombre de la sala?").toUpperCase();
-                                nueva_sala.nombre = nombre;
-                            break;
-                            case "Piso":
-                                respuesta_actualizacion =  prompt ("¿Dónde esta ubicada?");
-                                nueva_sala.piso = piso;
-                            break;
-                            case "Capacidad": 
-                            respuesta_actualizacion = prompt ("¿Cuantas personas pueden entrar?");
-                                nueva_sala.capacidad = capacidad;
-                            break;
-                            case "Proyector":
-                                respuesta_actualizacion = confirm ("¿Tiene proyector?")
-                                nueva_sala.proyector = proyector;
-                            break;
-                            default: 
-                                alert ("Escribí que dato querés cambiar");
-                        }
-                    }while(actualizarDato == "");
-
-                    // CORREGIR
-                    confirmar_sala = crear_confirmacion(nueva_sala);
-                    confirm (confirmar_sala);
-                    //
-                }
-        }else {
-            alert ("Dejaste un dato sin completar");
-        }
-    }while (validarDatos(false));
-
-    //let confirmar = crear_confirmacion();
+            switch (respuesta_instrucciones){
+                
+                case "1":
+                    //Pedir datos de sala
+                    pedirDatos_sala();
+                    break;
+                case "2":
+                    Salas_creadas();
+                    break;
+                case "3":
+                    //Borrar salas
+                case null:
+                    completar = false;
+                    break;
+                default: 
+                    alert ("Seleccioná una opción") ; 
+            }
 }
 
 /////////////////FUNCIONES/////////////////////////////
 
-function validarN (numeroCargado, condicionA, condicionB, pregunta, mensaje1, mensaje2, mensaje3){
-        let valor = numeroCargado;
-        do{
-            valor = parseInt(prompt(pregunta));
-            if(valor > condicionA){
-                alert (mensaje1);
-            }else if(valor < condicionB){
-                alert (mensaje2);
-            }else
-            console.log (mensaje3);
-        }while (valor > condicionA || valor < condicionB || valor == "" || valor == null || /\D/.test(valor));
+//Pedir datos de sala
+function pedirDatos_sala(){
+    let respueta = confirm ("¿Querés crear una sala nueva?");
 
-        let respuesta_valor = valor;
-        return respuesta_valor;
+    if (respueta) {
+        let nombre;
+        nombre = validarS();
+        
+        let piso;
+        piso = validarN(piso, 15,"El edificio tiene 15 pisos: \n\n¿En que piso esta la sala?", "❗El edificio tiene 15 pisos, elegí otro piso","❗Elegí un piso entre 1 y 15");
+            
+        let capacidad;
+        capacidad = validarN(capacidad, 20, "La capacidad máxima es 20 personas: \n\n¿Cuantas personas pueden entrar?", "❗La salas solo tienen capacidad hasta 20 personas", "❗La sala se debe reservar, minimamente, para 1 persona");
+
+        let proyector 
+        proyector = proyector_pregunta();
+
+        // Validar datos cargados
+
+        if (validarDatos(nombre) && validarDatos(capacidad) && validarDatos(piso)){
+
+            const nueva_sala = new Sala(nombre, capacidad, piso, proyector);
+
+                    let confirmar_sala = crear_confirmacion(nueva_sala);
+                    if (confirmar_sala) {
+                        salasGuardadas.push(nueva_sala);
+                        console.log(salasGuardadas);
+                        return escribirEn_documento(nueva_sala);
+                    }else{
+                        pedirDatos_sala();
+                        console.log(salasGuardadas);
+                    }
+        } 
     }
 
-    function proyector_detalle (dato){
-        let tieneProyector = dato;
-        tieneProyector = confirm("¿Tiene proyector?");
+//Valida el nombre
+    function validarS(){
+        let nombre;
+        do{
+            nombre = prompt("¿Cual va a ser el nombre de la sala?").toUpperCase();
+            if(!nombre){
+            alert ("Ingresá un nombre");
+            }
+            else{
+                return nombre;
+            }
+        }while(!nombre);
+    }
+//Valida el piso y la capacidad
+    function validarN (datoCargado, condicionA, pregunta, mensaje1, mensaje2, confirmacion){
+        let valor = datoCargado;
+            do{
+                valor = parseInt(prompt(pregunta));
+                if(valor > condicionA){
+                    alert (mensaje1);
+                }
+                if(valor < 1){
+                    alert (mensaje2);
+                }
+                else{       
+                    console.log (confirmacion);
+                }
+            }while (valor > condicionA || valor < 1 || valor == "" /*|| valor == null*/ || /\D/.test(valor));
+            
+        return valor;
+    }
+//determinar el valor del proyector
+    function proyector_pregunta (){
+        let tieneProyector = confirm("¿Tiene proyector?");
             if(tieneProyector){
-                alert ("Tiene proyector");
+                //alert ("Tiene proyector");
                 return true;
             }
             else{
-                alert ("No tiene proyector");
+                //alert ("No tiene proyector");
                 return false;
             }
         }        
 
+//Confirmación de datos cargados        
     function crear_confirmacion(datoSala){
-        let mensaje = "";
-        mensaje = mensaje + "¿Los datos cargados son correctos? \n";
-        mensaje = mensaje + "\n Nombre: " + datoSala.nombre;
-        mensaje = mensaje + "\n Piso: " + datoSala.piso;
-        mensaje = mensaje + "\n Capacidad: " + datoSala.capacidad;
-        mensaje = mensaje + "\n ¿Tiene proyector?: " + datoSala.mostrar_proyector() ;
+        let mensaje = "¿Los datos cargados son correctos? \n";
+        mensaje += "\n Nombre: " + datoSala.nombre;
+        mensaje += "\n Piso: " + datoSala.piso;
+        mensaje += "\n Capacidad: " + datoSala.capacidad + " personas";
+        mensaje += "\n ¿Tiene proyector?: " + datoSala.proyector;
 
         let respuesta = confirm(mensaje);
 
         return respuesta;
     }
 
-
+//Valida que los datos fueron cargados
     function validarDatos(dato){
         if(!dato){
             return false;
@@ -120,33 +131,40 @@ function validarN (numeroCargado, condicionA, condicionB, pregunta, mensaje1, me
         }
         return true;
     }
+//Escribir salas en el documento
+    function escribirEn_documento(salaI){
+        //alert ("Sala " + salaI.mostrar_info() + " creada🤘");
+        document.write (salaI.mostrar_infoCompleta() + "<br>");
+        console.log(salaI);
+}
 
-    function actualizarDato(numeroDato){
-        let actualizarDato = numeroDato;
-        let nueva_sala
-                    do{
-                        actualizarDato = prompt("¿Que dato queres actualizar? \n - Nombre \n - Piso \n - Capacidad \n - Proyector");
-                        switch (actualizarDato){
-                            case "Nombre":
-                                respuesta_actualizacion = prompt ("¿Cual va a ser el nombre de la sala?").toUpperCase();
-                                nueva_sala.nombre = nombre;
-                            break;
-                            case "Piso":
-                                respuesta_actualizacion =  prompt ("¿Dónde esta ubicada?");
-                                nueva_sala.piso = piso;
-                            break;
-                            case "Capacidad": 
-                            respuesta_actualizacion = prompt ("¿Cuantas personas pueden entrar?");
-                                nueva_sala.capacidad = capacidad;
-                            break;
-                            case "Proyector":
-                                respuesta_actualizacion = confirm ("¿Tiene proyector?")
-                                nueva_sala.proyector = proyector;
-                            break;
-                            default: 
-                                alert ("Escribí que dato querés cambiar");
-                        }
-                    }while(actualizarDato == "");
-        
-        return actualizarDato;
+
+
+    //Borrar salas
+    function borrar_salas(){
+
+    }
+
+}
+
+//Ver salas creadas
+function Salas_creadas(){
+
+    if(buscarSalas(true)){
+
+        alert(salasGuardadas);
+    }
+
+    function buscarSalas(){
+    
+        if (salasGuardadas.length == 0){
+
+            alert("Por el momento no hay salas creadas");
+
+            return false;
+
+        }else{
+            return true;
+        }
+    }
 }
--- JS/salas.js
+++ JS/salas.js
@@ -1,3 +1,5 @@
+// Este es el array donde se guardan las salas creadas
+const salasGuardadas = new Array();
 // Creaccion de salas
 class Sala {
 
@@ -5,25 +7,48 @@ class Sala {
         this.nombre = nombre;
         this.capacidad = parseInt(capacidad);
         this.piso = parseInt(piso);
-        this.estado = true;
-        this.proyector = undefined;
-
-
+        this.proyector = false;
     }
     mostrar_info(){
         return this.nombre;
     }
    
     mostrar_proyector(){
-        if(this.proyector){
-            return "Si";
-        }else{
+        if(this.proyector == false){
             return "No";
+        }else{
+            return "Sí";
         }
     }
+
     mostrar_infoCompleta(){
-        return "Se creo la sala " + this.nombre + " - " + this.estado + "<br>"
+        return this.nombre + "<br>"
         + "Capacidad: " + this.capacidad + " personas" + " - " + "Proyector: " + this.proyector + "<br>"
         + "Ubicado en el piso N° " + this.piso;
     }
+
+
+    set_nombre(nuevo_nombre){
+        this.nombre = nuevo_nombre;
+    }
+
+    //Cambiar proyector a false
+    set_proyector(cambio_proyector){
+        this.proyector = cambio_proyector;
+    }
+
+
 }
+
+
+
+
+
+
+
+/* ¿Como hago para al reservar un horario y un dia para la sala quede guardada la info?
+ Al reservar un lunes de 10 a 11 la sala creada deberia bloquear ese dato.... como lo hago?
+ 
+ 
+ 
+ */
